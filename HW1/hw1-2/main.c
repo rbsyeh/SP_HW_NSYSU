227089fe1af192b269fc02fb831d72a6bf12edd8
Wen-Ching Yeh <kp0310j@gmail.com>
2022-01-02 18:40:59 +0800

    typesetting

--- HW1/hw1-2/main.c
+++ HW1/hw1-2/main.c
@@ -4,41 +4,41 @@
 
 int main( int argc, char* argv[]){
         
-		if(argc==3){
+	if(argc==3){
 
-		    char filename1[20];
-		    strcpy (filename1, argv[1]);
-		    char filename2[20];
-		    strcpy (filename2, argv[2]);
+		char filename1[20];
+		strcpy (filename1, argv[1]);
+		char filename2[20];
+		strcpy (filename2, argv[2]);
 
-		    FILE* input_file  = fopen(filename1, "rb");
+		FILE* input_file  = fopen(filename1, "rb");
 
-		    if (!input_file){
-				perror("fopen");
-				exit(EXIT_FAILURE);
-		    }
+		if (!input_file){
+			perror("fopen");
+			exit(EXIT_FAILURE);
+		}
 
-		    int a=0;
+		int a=0;
 
-		    fseek(input_file,0,SEEK_END);
- 			  long int size=ftell(input_file); // get size of file in bytes
- 			  fseek(input_file,0,SEEK_SET);
+		fseek(input_file,0,SEEK_END);
+		long int size=ftell(input_file); // get size of file in bytes
+		fseek(input_file,0,SEEK_SET);
 
- 			  FILE* output_file  = fopen(filename2, "wb+");
+		FILE* output_file  = fopen(filename2, "wb+");
 
 
-        if (!output_file){
-            perror("fopen");
-            exit(EXIT_FAILURE);
-        }
+		if (!output_file){
+		    perror("fopen");
+		    exit(EXIT_FAILURE);
+		}
 		    
-		    for(int i=0; i<size; i++){
-    			  fread(&a, sizeof(int), 1, input_file); //read one int (sizeof(int)!=1 byte)
-    			  fwrite(&a, sizeof(int), 1, output_file);
-  			}
+		for(int i=0; i<size; i++){
+			fread(&a, sizeof(int), 1, input_file); //read one int (sizeof(int)!=1 byte)
+			fwrite(&a, sizeof(int), 1, output_file);
+		}
 
-        fclose(input_file);
-		    fclose(output_file);
-		} 
+        	fclose(input_file);
+		fclose(output_file);
+	} 
 	return 0;
 }
